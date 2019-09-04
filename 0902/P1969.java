e61b466205c4d66c995457cd0c92f2f59e51cf52
Richard <liuyongcheng@rongcloud.cn>
2019-09-04 11:05:09 +0800

    0903

--- 0902/P1969.java
+++ 0902/P1969.java
@@ -5,44 +5,42 @@
  * 2 3 4 入栈，1 比4小，不能入栈
  * 出栈4，直到1可以入栈，
  */
-import java.util.Scanner;
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.*;
 public class P1969 {
-    private static int n;
-    private static int[] h=new int[100000+10];
-    private static int ans;
+	private static int n;
+	private static int[] h = new int[100000 + 10];
+	private static int ans;
 	public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        for(int i=1;i<=n;i++)
-            h[i]=sc.nextInt();
-        Deque<Integer> q=new ArrayDeque<>();
-        for(int i=1;i<=n;i++){
-        	if(q.isEmpty()||h[i]>q.peekFirst()){
-        			q.push(h[i]);
-        	}else if(h[i]==q.peekFirst()){
-        		continue;
-        	}else{
-        		Integer b=-1;
-        		do{
-        			if(q.isEmpty()) {
-        				b=0;
-        				break;
-        			} else {
-	        			b=q.pop();
-                        if(q.isEmpty())
-	        			    ans+=b;
-                        else
-                            ans+=b-q.peekFirst();
-        			}
-        		}while(b>h[i]);
-        		if(b<h[i]){
-        			    q.push(h[i]);
-        		}
-        		else continue;
-        	}
-        }
-        System.out.println(ans);
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = 1; i <= n; i++)
+			h[i] = sc.nextInt();
+		List<Integer> q = new ArrayList<Integer>();
+		for (int i = 1; i <= n + 1; i++) {
+			if (q.isEmpty() || h[i] > q.get(q.size() - 1)) {
+				q.add(h[i]);
+			} else if (h[i] == q.get(q.size() - 1)) {
+				continue;
+			} else {
+				pop(q, h[i]);
+			}
+		}
+		System.out.println(ans);
+	}
+
+	private static void pop(List<Integer> q, int h) {
+		int max = 0;
+		if (q.size() > 1) {
+			max = Math.max(q.get(q.size() - 2), h);
+		} else {
+			max = h;
+		}
+		ans += q.get(q.size() - 1) - max;
+		q.remove(q.size() - 1);
+		if (!q.isEmpty() && q.get(q.size() - 1) > h) {
+			pop(q, h);
+		} else {
+			q.add(h);
+		}
 	}
 }
---
+++ 0903/README.md
@@ -0,0 +1,9 @@
+#1. 方格取数
+https://www.luogu.org/problem/P1004
+#2.https://www.luogu.org/problem/P1018
+f[i][j]表示考虑前i个字符,插入j个乘号的，使得成绩最大
+空间复杂度
+时间复杂度
+状态复杂度f[i][j]
+转移复杂度
+#3. https://www.luogu.org/problem/P1078
